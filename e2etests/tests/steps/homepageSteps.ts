b7fc2ecda6aa4ae519db33dab1047bc0aad1c013
matthewmjm <matthewmjm@yahoo.com>
2025-08-25 18:48:48 -0600

    user on homepage test

--- e2etests/tests/steps/homepageSteps.ts
+++ e2etests/tests/steps/homepageSteps.ts
@@ -1,7 +1,14 @@
 import { Given, Then, When } from "@cucumber/cucumber";
+import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
 
 Given("User is on homepage", async function () {
 	console.log("----------User is on the homepage----------");
+	let browser: Browser = await chromium.launch({ headless: false, channel: "chromium", args: ["--start-maximized"] });
+	let browserContext: BrowserContext = await browser.newContext({ viewport: null, javaScriptEnabled: true });
+	let page: Page = await browserContext.newPage();
+	await page.goto("https://ecommerce-playground.lambdatest.io/");
+	await page.close();
+	await browser.close();
 });
 
 When("User enters login details", async function () {
